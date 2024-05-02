7a052c5f6c4c14c5db603caf69d1b68d77e9affe
Abhinil-07 <abhinilnath10@gmail.com>
2024-05-02 21:13:42 +0530

    fixed cors issues

--- app.ts
+++ app.ts
@@ -11,7 +11,11 @@ const app: Application = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(cors());
+const corsOptions = {
+  origin: "https://avenir-admin-panel.vercel.app",
+};
+
+app.use(cors(corsOptions));
 
 app.get("/", async (req, res) => {
   res.status(200).json("Health check endpoint for the server");
