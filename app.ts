6cc16b2caa1d6a1edd494c691b0dbf2b4878734f
Abhinil Nath <85664966+Abhinil-07@users.noreply.github.com>
2024-05-03 21:05:07 +0530

    Fixed cors v2

--- app.ts
+++ app.ts
@@ -12,11 +12,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 const corsOptions = {
-  origin: "https://avenir-admin-panel.vercel.app",
+  origin: ["https://avenir-admin-panel.vercel.app", "http://localhost:3000"],
 };
 
 app.use(cors(corsOptions));
-
 app.get("/", async (req, res) => {
   res.status(200).json("Health check endpoint for the server");
 });
